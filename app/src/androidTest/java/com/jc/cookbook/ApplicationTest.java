f66f6bdf0c0f1c9f87acfacd4cc343330d2db459
Tconan99 <627594232@qq.com>
2016-03-16 08:30:25 +0800

    测试文件提交是否正常

--- app/src/androidTest/java/com/jc/cookbook/ApplicationTest.java
+++ app/src/androidTest/java/com/jc/cookbook/ApplicationTest.java
@@ -2,6 +2,7 @@ package com.jc.cookbook;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.util.Log;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -9,5 +10,9 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+        int a = 0;
+        int b = a + 3;
+        int c = a + b + 7;
+        Log.i("c=", "" + c);
     }
 }
